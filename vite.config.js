5e0db53528515d543a9ffb4637e66745546f118d
samghoreyshi <samghoreyshi@outlook.com>
2024-10-18 00:22:47 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to all network interfaces
+    port: 3000, // Specify any port you prefer
+    open: true, // Optional: open browser on start
+  },
+});
